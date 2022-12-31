4d8072ea18a2a6aeac8219df1684f9dbb521fbed
SuperNinjaFat <paulmlindberg@gmail.com>
2022-12-31 15:25:53 -0600

    change SITE_ROOT to match the website's root

--- core/initialize.php
+++ core/initialize.php
@@ -2,7 +2,7 @@
 //initialize core files
 
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'Applications' . DS . 'MAMP' . DS . 'htdocs' . DS . 'BookStore');
+defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'home1' . DS . 'paulmlin' . DS . 'public_html' . DS . 'Bookstore-API');
 
 defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT.DS.'includes');
 defined('CORE_PATH')? null : define('CORE_PATH', SITE_ROOT.DS.'core');
